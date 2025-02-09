799f81480ed493ef390fb547a9fcb748047b3f40
Dominik Künkele <contact@dominik-kuenkele.de>
2025-02-09 17:57:50 +0100

    fix api

--- api_prediction.py
+++ api_prediction.py
@@ -1,17 +1,14 @@
-from flask import Flask, request, jsonify
-
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
-from torchvision.models import ResNet101_Weights
-
 import argparse
 
+import torch
+from flask import Flask, jsonify, request
+from PIL import Image
+from torchvision.models import ResNet101_Weights
 
 app = Flask(__name__)
 
 # Argument Parser
-parser = argparse.ArgumentParser()    
+parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, help="path to the inference model")
 args = parser.parse_args()
 
@@ -19,20 +16,21 @@ args = parser.parse_args()
 model = torch.load(args.model_path)
 model.eval()  # Wichtig für Inferenz
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify({'error': 'Keine Datei hochgeladen'})
 
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({'error': 'Keine Datei ausgewählt'})
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return jsonify({"error": "Keine Datei hochgeladen"})
+
+        file = request.files["file"]
+        if file.filename == "":
+            return jsonify({"error": "Keine Datei ausgewählt"})
 
         try:
             # Bild verarbeiten
-            image = Image.open(file).convert('RGB')
-            
+            image = Image.open(file).convert("RGB")
+
             longer_side = max(image.size[0], image.size[1])
             new_size = (longer_side, longer_side)
             new_im = Image.new("RGB", new_size)
@@ -40,21 +38,22 @@ def predict():
             new_im.paste(image, box)
 
             preprocessed_image = ResNet101_Weights.IMAGENET1K_V2.transforms()(new_im)
-            
+
             image_final = preprocessed_image.unsqueeze(0)  # Batch-Dimension hinzufügen
 
             # Inferenz
             with torch.no_grad():
                 output = model(image_final)
-                _, predicted_class = torch.argmax(output, 1)
+                predicted_class = torch.argmax(output, 1)
 
             # Ergebnis zurückgeben
-            return jsonify({'class': int(predicted_class)})
+            return jsonify({"class": int(predicted_class)})
 
         except Exception as e:
-            return jsonify({'error': str(e)})
+            return jsonify({"error": str(e)})
 
-    return jsonify({'error': 'Falsche Methode'})
+    return jsonify({"error": "Falsche Methode"})
 
-if __name__ == '__main__':
-        app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
